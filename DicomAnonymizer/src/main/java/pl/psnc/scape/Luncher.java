e6cc266e131cedd64f56a6184be051fb099a77b5
edwin <edwin@man.poznan.pl>
2013-11-28 10:03:16 +0100

    help printing

--- DicomAnonymizer/src/main/java/pl/psnc/scape/Luncher.java
+++ DicomAnonymizer/src/main/java/pl/psnc/scape/Luncher.java
@@ -25,5 +25,43 @@ public class Luncher {
     	else if((args.length == 2) && (args[0].compareTo("-personalize_dir") == 0)){
             Personalizer.personalizeDir(args[1]);
     	}
+    	else if((args.length == 1) && (args[0].compareTo("-help") == 0)){
+    		printHelp();
+    	}
+    	else {
+    		printShortHelp();
+    	}
     }
+
+	private static void printShortHelp() {
+		System.out.println("This tool removes DICOM tags (specified in the configuration file: config.properties) and add DICOM tags (specified in text file) to from/to the given DICOM file(s).");
+		System.out.println();
+		System.out.println("Parameters:");
+		System.out.println("-anonymize <dicom_file> <anonymized_tag_file>");
+		System.out.println(" anonymizes DICOM file and saves anonymized tag values in text file.");
+		System.out.println("-anonymize_dir <directory> ");
+		System.out.println("-personalize <dicom_file> <anonymized_tag_file>");
+		System.out.println("-personalize_dir <directory>");
+		System.out.println("-help");
+		System.out.println(); 
+	}
+	
+	private static void printHelp() {
+		System.out.println("This tool removes DICOM tags (specified in the configuration file: config.properties) and add DICOM tags (specified in text file) to from/to the given DICOM file(s).");
+		System.out.println();
+		System.out.println("Parameters:");
+		
+		System.out.println("-anonymize <dicom_file> <anonymized_tag_file>");
+		System.out.println(" anonymizes DICOM file and saves anonymized tag values in text file.");
+		System.out.println();
+		System.out.println("-anonymize_dir <directory> ");
+		System.out.println(" anonymizes all DICOM files in specified directory and subdirectories and saves anonymized tag values in text file for each DICOM file.");
+		System.out.println();
+		System.out.println("-personalize <dicom_file> <anonymized_tag_file>");
+		System.out.println(" presonalizes DICOM file.");
+		System.out.println();
+		System.out.println("-personalize_dir <directory>");
+		System.out.println(" presonalizes all DICOM files in specified directory and subdirectories");
+		System.out.println(); 
+	}
 }
